11df7f495d44c7eeb9ba71e9af1757c7a24182cc
roman <roman@DESKTOP-NC1K615>
2019-07-02 14:32:33 -0500

    listo we

--- Automatas/src/Prueba/M.java
+++ Automatas/src/Prueba/M.java
@@ -3,7 +3,8 @@ package Prueba;
 public class M {
 
     public static void main(String[] args) {
-        System.out.println("lo se");
+        System.out.println("me la pelas bien duro");
+        
     }
     
 }
